98d07f42c8c91c1796908946c6806d24b1a59a74
TrevorMcB <151088808+TrevorMcB@users.noreply.github.com>
2025-11-18 18:37:43 -0600

    Update backendMySql.py
    updated

--- backendMySql.py
+++ backendMySql.py
@@ -3,35 +3,72 @@ import mysql.connector
 
 def get_con():
     TheSqlDB= mysql.connector.connect(
-        host="localhost",
-        user="username",
-        password="pasword",
-        database="nameOfDB"
+        host="127.0.0.1",
+        port=3307,
+        user="root",
+        password="root",
+        database="mydb"
     )
+    return TheSqlDB
 
-#def get_user():
+#def get_user(): finds user
+#con=get_con()
+#curs=con.cursor()
+#may have to call to get username and password from html front end
+#inSQL="SELECT user (username,loginpsswd) VALUES (%s, %s)"
+#curs.execute(inSQL,(username,loginPsswd,userID))
+#curs.commit()
+#curs.close()
+#con.close()
 
 
-#def get_account():
+#def get_account(): finds account
+#con=get_con()
+#curs=con.cursor()
+#inSQL="SELECT account ("what do i use to look for, user and service?") VALUES (%s, %s)"
+#curs.execute(inSQL,(username,loginPsswd,userID))
+#curs.commit()
+#curs.close()
+#con.close()
+
 
 #def set_user_password():
-#def set_user_username():
+#def set_user_username(): used for login to maanager
+
 #def set_account_password():
-#def set_account_username():
+#con=get_con()
+#curs=con.cursor()
+#inSQL="SELECT user (username,loginpsswd) VALUES (%s, %s)"
+#curs.execute(inSQL,(username,loginPsswd,userID))
+#curs.commit()
+#curs.close()
+#con.close()
+
+
+#def set_account_username(): what is used for account login
+#con=get_con()
+#curs=con.cursor()
+#inSQL="SELECT user (username,loginpsswd) VALUES (%s, %s)"
+#curs.execute(inSQL,(username,loginPsswd,userID))
+#curs.commit()
+#curs.close()
+#con.close()
+
+
 def set_user( username:str,loginPsswd: str, userID: int):
     con=get_con()
     curs=con.cursor()
-    inSQL="INSERT INTO user (username,loginpsswd,userID) VALUES (%s, %s, %s)"
+    inSQL="INSERT INTO 'user' (username,loginPsswd,userID) VALUES (%s, %s, %s)"
     curs.execute(inSQL,(username,loginPsswd,userID))
     curs.commit()
-    con.close()
     curs.close()
+    con.close()
 
 def set_account( entryID: int, user_userID: int, serviceUsername: str,serviceName:str, serviceCategory: str, encryptPassword: str):
     con=get_con()
     curs=con.cursor()
     inSQL="INSERT INTO vault (entryID,user_userID, serviceUsername, serviceName, serviceCategory, encryptPassword) VALUES (%s, %s, %s,%s, %s, %s)"
-    curs.execute(inSQL,(entryID,user_userID, serviceUsername, serviceName, serviceCategory, encryptPassword))   
+    curs.execute(inSQL,(entryID, user_userID, serviceUsername, serviceName, serviceCategory, encryptPassword))   
     curs.commit()
-    con.close()
-    curs.close()   
+    curs.close()
+    con.close()  
